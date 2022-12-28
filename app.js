b196a55910da22de52a97e8d121243fb1c46184c
Matias Guarnera <matiasguarnera@gmail.com>
2022-12-28 06:56:04 -0300

    Type events added. Id, title, desc, price and date

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery{
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation{
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -29,8 +46,13 @@ app.use('/graphql', graphqlHTTP({
         },
 
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.title,
+                description: args.description,
+                price: +args.price, 
+                date: new Date().toISOString()
+            }
         }
     },
     graphiql: true
