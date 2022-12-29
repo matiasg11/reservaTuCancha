7f84fe44c354ecf094eca848ab4fe8689aab851b
Matias Guarnera <matiasguarnera@gmail.com>
2022-12-29 07:22:45 -0300

    Added mongoose. Dependency errors avoided

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql'); //Middleware function that we call later
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose')
 
 const app = express();
 
