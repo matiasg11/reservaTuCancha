76cd3e7e82cdc8aed31f36cb5d59c8e148497568
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-04 19:34:48 -0300

    Event Schema w/ title, desc, date and price

--- models/event.js
+++ models/event.js
@@ -6,5 +6,18 @@ const eventSchema = new Schema({
     title: {
         type: String,
         required: true,
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    price:{
+        type: Number,
+        required:true,
+    },
+    date: {
+        type: Date,
+        required: true
     }
+
 });
