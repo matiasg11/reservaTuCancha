09977a76360d4b1e7d25364d9a6e980ab5b815a4
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-06 18:18:37 -0300

    Linking Events and Users

--- app.js
+++ app.js
@@ -21,12 +21,14 @@ app.use('/graphql', graphqlHTTP({
             description: String!
             price: Float!
             date: String!
+            creator: User!
         }
 
         type User {
             _id: ID!
             email: String!
             password: String
+            createdEvents: [Event!]
         }
 
         input EventInput{
@@ -59,9 +61,15 @@ app.use('/graphql', graphqlHTTP({
     rootValue: {
         events: () =>{
             return Event.find()
+                .populate('creator')
                 .then(events => {
                     return events.map(event => {
-                        return {...event._doc, _id: event._doc._id.toString()}
+                        return {...event._doc,
+                                 _id: event._doc._id.toString(),
+                                creator: {
+                                    ...event._doc.creator._doc,
+                                    _id: event._doc.creator.id
+                                } }
                     })
                 })
                 .catch(err => {throw err})
