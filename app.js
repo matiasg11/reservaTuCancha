664ec46291eb14f31747e8aca2beb67709010733
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-07 19:04:35 -0300

    Schema's indexjs

--- app.js
+++ app.js
@@ -47,50 +47,7 @@ const user = userId => {
 
 
 app.use('/graphql', graphqlHTTP({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-            creator: User!
-        }
-
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-            createdEvents: [Event!]
-        }
-
-        input EventInput{
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput{
-            email: String!
-            password: String!
-        }
-
-        type RootQuery{
-            events: [Event!]!
-        }
-
-        type RootMutation{
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `), //point to a valid graphql schema
-    //Has all the resolver functions. Need to match the schema endpoints by name
+   
     rootValue: {
         events: () =>{
             return Event.find()
