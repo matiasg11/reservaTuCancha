f8bb5fc502d100753cbe4ba9d0a79d65d2420db3
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-10 19:38:52 -0300

    auth corrected and booking refactored

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,33 +1,24 @@
-const Event = require('../../models/event');
-const Booking = require('../../models/booking');
-const {transformBooking, transformEvent } = require('./merge');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/user');
 
 module.exports = {
-  bookings: async () => {
+  createUser: async args => {
     try {
-      const bookings = await Booking.find();
-      return bookings.map(booking => {
-        return transformBooking(booking);
+      const existingUser = await User.findOne({ email: args.userInput.email });
+      if (existingUser) {
+        throw new Error('User exists already.');
+      }
+      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+      const user = new User({
+        email: args.userInput.email,
+        password: hashedPassword
       });
-    } catch (err) {
-      throw err;
-    }
-  },
-  bookEvent: async args => {
-    const fetchedEvent = await Event.findOne({ _id: args.eventId });
-    const booking = new Booking({
-      user: '5c0fbd06c816781c518e4f3e',
-      event: fetchedEvent
-    });
-    const result = await booking.save();
-    return transformBooking(result);
-  },
-  cancelBooking: async args => {
-    try {
-      const booking = await Booking.findById(args.bookingId).populate('event');
-      const event = transformEvent(booking.event);
-      await Booking.deleteOne({ _id: args.bookingId });
-      return event;
+
+      const result = await user.save();
+
+      return { ...result._doc, password: null, _id: result.id };
     } catch (err) {
       throw err;
     }
