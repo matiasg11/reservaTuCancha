c88cb8ed9518dcdb6eff7c1880ae4fb2e02ebe7b
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-11 20:08:16 -0300

    Only letting some changes if the user is authenticated

--- app.js
+++ app.js
@@ -5,16 +5,11 @@ const mongoose = require('mongoose');
 
 const graphQLSchema = require('./graphql/schema/index')
 const graphQLResolvers = require('./graphql/resolvers/index')
-
+const isAuth = require('./middleware/is-auth')
 
 const app = express();
 
 app.use(bodyParser.json())
-
-
-
-
-
 
 app.use('/graphql', graphqlHTTP({
     schema: graphQLSchema,
